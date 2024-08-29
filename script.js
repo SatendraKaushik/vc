659c821e56858f7cf19955cd9a74b539a952968b
Satendra Kaushik <126915651+SatendraKaushik@users.noreply.github.com>
2024-08-29 21:46:22 +0530

    Update script.js

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ const remoteVideo = document.getElementById('remoteVideo');
 let localStream;
 let peerConnection;
 const configuration = { 'iceServers': [{ 'urls': 'stun:stun.l.google.com:19302' }] };
-const signalingServerUrl = 'wss://videocalling-lcwm.onrender.com/';
+const signalingServerUrl = 'wss:/https://videocalling-1nud.onrender.com/';
 const signalingServer = new WebSocket(signalingServerUrl);
 
 startCallButton.addEventListener('click', async () => {
